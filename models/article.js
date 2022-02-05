04474f8502381adffbb987afeca14e82a953b545
charlesncn <charowcjn@gmail.com>
2022-02-05 07:01:23 +0300

    updated show page

--- models/article.js
+++ models/article.js
@@ -59,6 +59,9 @@ articleSchema.pre('validate', function(next){
     if (this.markdown) {
         this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
     }
+    if (this.description) {
+        this.sanitizedHtml = dompurify.sanitize(marked(this.description))
+    }
     next()
 })
 
